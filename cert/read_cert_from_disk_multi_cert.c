a8e1670008c778bb262a3322df55b4a4bfb3e3fb
yk <yksring@gmail.com>
2022-07-15 16:42:32 +0800

    add load pem from file

--- cert/read_cert_from_disk_multi_cert.c
+++ cert/read_cert_from_disk_multi_cert.c
@@ -4,34 +4,42 @@
 #include <openssl/pem.h>
 
 /* 读取含有多个cert的证书, 存入othercerts*/ 
+/*
+ * cat root.pem intermediate.pem leaf.pem >CA.pem
+ *
+ * reference crypto/x509/by_file.c
+ *
+ * gcc read_cert_from_disk_multi_cert.c -o read_cert_from_disk_multi_cert -lcrypto 
+ *
+ */
 int main(int argc, char **argv)
 {
-		char *name = "OSCP_KEY.pem";
-		BIO *certs = NULL;
-		STACK_OF(X509_INFO) *allcerts = NULL;
-		STACK_OF(X509)* othercerts = NULL;
-		int ret = 0;
 
-		OpenSSL_add_all_algorithms();
-		//certs = BIO_new_file(name, "r");
-		certs = BIO_new_file(argv[1], "r");
-		othercerts = sk_X509_new_null();
+		char *file = "CA.pem";
+		STACK_OF(X509_INFO) *inf;
+		STACK_OF(X509)* chain;
+		X509_INFO *itmp;
+		BIO *in;
+		int i, count = 0;
+		
+		in = BIO_new_file(file, "r");
 
-		allcerts = PEM_X509_INFO_read_bio(certs, NULL, NULL, NULL);
-		for(int i = 0; i < sk_X509_INFO_num(allcerts); i++)
-		{
-				X509_INFO *xi = sk_X509_INFO_value(allcerts, i);
-				if(xi->x509)
+		inf = PEM_X509_INFO_read_bio(in, NULL, NULL, "");
+		BIO_free(in);
+
+		for (i = 0; i <sk_X509_INFO_num(inf); i++) {
+			itmp = sk_X509_INFO_value(inf, i);
+				if(itmp->x509)
 				{
-						sk_X509_push(othercerts, xi->x509);
-						ret++;
-						xi->x509 = NULL;
+						sk_X509_push(chain, itmp->x509);
+						count++;
 				}
+
 		}
 
-		sk_X509_INFO_pop_free(allcerts, X509_INFO_free);
-		BIO_free(certs);
+
+		sk_X509_INFO_pop_free(inf, X509_INFO_free);
 
 
-		return 0;
+		return count;
 }
