3e4b592c0dd67504fa216cafc794d7f25f23a5b5
NanFeng009 <yksring@gmail.com>
2023-01-31 20:36:04 +0800

    generate custom extension

---
+++ cert/add_custom_extension.c
@@ -0,0 +1,33 @@
+#include <openssl/asn1.h>
+#include <openssl/x509.h>
+
+int main()
+{
+    X509_EXTENSION *ext = NULL;
+    ASN1_OCTET_STRING *data = NULL;
+    ASN1_OBJECT *obj = NULL;
+
+    // Define the Object Identifier (OID) for the custom extension
+    obj = OBJ_txt2obj("1.2.3.4.5.6.7.8", 0);
+
+    // Convert the string to an ASN1_OCTET_STRING
+    data = ASN1_OCTET_STRING_new();
+    ASN1_OCTET_STRING_set(data, (unsigned char *)"Custom Extension Data", 23);
+
+    // Create the X509_EXTENSION with the OID and ASN1_OCTET_STRING
+    ext = X509_EXTENSION_create_by_OBJ(NULL, obj, 0, data);
+    
+    // Add extension to cert
+    X509_add_ext(x509cert, ext, -1)
+    
+    // Free the ASN1_OCTET_STRING and ASN1_OBJECT
+    ASN1_OCTET_STRING_free(data);
+    ASN1_OBJECT_free(obj);
+
+    // Use the X509_EXTENSION as needed
+
+    // Free the X509_EXTENSION when done
+    X509_EXTENSION_free(ext);
+
+    return 0;
+}
